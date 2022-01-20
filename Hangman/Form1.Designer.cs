12ba75acaa6570a8ff8b5288d15ee39d53ac5f53
Harrison Baxter <2334385@live.barnsley.ac.uk>
2022-01-20 13:50:26 +0000

    Add project files.

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnLetter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(491, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hangman";
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(604, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 378);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 356);
+            this.panel1.TabIndex = 0;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(18, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(580, 244);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Word Length:";
+            this.label2.UseWaitCursor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtWord);
+            this.groupBox3.Controls.Add(this.txtLetter);
+            this.groupBox3.Controls.Add(this.btnWord);
+            this.groupBox3.Controls.Add(this.btnLetter);
+            this.groupBox3.Location = new System.Drawing.Point(18, 301);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(580, 128);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.UseWaitCursor = true;
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(384, 46);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(149, 23);
+            this.txtWord.TabIndex = 3;
+            this.txtWord.UseWaitCursor = true;
+            // 
+            // txtLetter
+            // 
+            this.txtLetter.Location = new System.Drawing.Point(148, 46);
+            this.txtLetter.MaxLength = 1;
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.Size = new System.Drawing.Size(82, 23);
+            this.txtLetter.TabIndex = 2;
+            this.txtLetter.UseWaitCursor = true;
+            // 
+            // btnWord
+            // 
+            this.btnWord.Location = new System.Drawing.Point(246, 36);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(111, 59);
+            this.btnWord.TabIndex = 1;
+            this.btnWord.Text = "Submit Word";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.UseWaitCursor = true;
+            // 
+            // btnLetter
+            // 
+            this.btnLetter.Location = new System.Drawing.Point(17, 36);
+            this.btnLetter.Name = "btnLetter";
+            this.btnLetter.Size = new System.Drawing.Size(111, 59);
+            this.btnLetter.TabIndex = 0;
+            this.btnLetter.Text = "Submit Letter";
+            this.btnLetter.UseVisualStyleBackColor = true;
+            this.btnLetter.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 452);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnLetter;
+    }
+}
+
